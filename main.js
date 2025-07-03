5c458cdf244a43cf2f1ee3badc60f341c96a63d2
Xavier <xavc3141@gmail.com>
2025-07-03 12:26:04 -0800

    hi

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 
 function writeHello() {
   clearCanvas();
+  resizeCanvas(300,500);
   ctx.font = "30px Arial";
   ctx.fillStyle = "blue";
   ctx.fillText("Hello", 50, 100);
@@ -10,6 +11,7 @@ function writeHello() {
 
 function writeWorld() {
   clearCanvas();
+  resizeCanvas(350,400);
   ctx.font = "30px Arial";
   ctx.fillStyle = "green";
   ctx.fillText("World", 200, 100);
@@ -17,4 +19,9 @@ function writeWorld() {
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function resizeCanvas(x,y) {
+  canvas.height = y;
+  canvas.width = x;
 }
