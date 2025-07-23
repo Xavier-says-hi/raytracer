cd850cc4cef018eec1d4197df09ebbb10e04825b
Xavier <xavc3141@gmail.com>
2025-07-22 22:32:30 -0500

    hjkl

--- htmlcomm.js
+++ htmlcomm.js
@@ -1,23 +1,25 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+window.onload = function() {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
 
-function main() {
-  clearCanvas();
-  resizeCanvas(300,500);
-  ctx.font = "30px Arial";
-  ctx.fillStyle = "blue";
-  ctx.fillText("Hello", 50, 100);
-}
+  function main() {
+    clearCanvas();
+    resizeCanvas(300,500);
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "blue";
+    ctx.fillText("Hello", 50, 100);
+  }
 
-function stop() {
-  clearCanvas();
-}
+  function stop() {
+    clearCanvas();
+  }
 
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 
-function resizeCanvas(x,y) {
-  canvas.height = y;
-  canvas.width = x;
+  function resizeCanvas(x,y) {
+    canvas.height = y;
+    canvas.width = x;
+  }
 }
